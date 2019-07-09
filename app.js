a2f07870a77b8ab4a8e03fc5da90594492ed1f80
Max <49519905+Roixxx@users.noreply.github.com>
2019-07-09 11:31:55 +0500

    added the scroll function on js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 $(function() {
 
+	let	header = $("#header");
+	let	header_top = $("#header_top");
+	let	header_height = header.innerHeight();
+	let scrollPos = $(window).scrollTop();
+
+	$(window).on("scroll", function() {
+
+		scrollPos = $(this).scrollTop();
+
+		if (scrollPos > header_height) {
+			header_top.addClass("fixed");
+		} else {
+			header_top.removeClass("fixed");
+		}
+	})
+
+
+
+
+
+	header_top.addClass("fixed");
 });
